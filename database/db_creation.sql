74062a7bd03c60bb63a4e3a40dca910721411264
eromeroc97 <45209976+eromeroc97@users.noreply.github.com>
2024-10-09 18:08:50 +0200

    Update db_creation.sql

--- database/db_creation.sql
+++ database/db_creation.sql
@@ -2,3 +2,4 @@ CREATE DATABASE alba_ethereum;
 
 CREATE USER alarcos WITH PASSWORD 'alarcos';
 GRANT ALL PRIVILEGES ON DATABASE alba_ethereum TO alarcos;
+GRANT USAGE, CREATE ON SCHEMA public TO alarcos;
